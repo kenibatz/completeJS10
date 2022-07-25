731adcfc04e2657429f40766f57df1c4b676adba
kenibatz <kenny.batallones@gmail.com>
2022-07-25 17:41:49 +0800

    closures

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // Closures
+console.log(`\n*** Closures are preserved variable environments ***`);
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`passengerCount: ${passengerCount}`);
+  };
+};
+
+const booker = secureBooking();
+
+console.log(``);
+
+booker();
+booker();
+booker();
+
+console.log(`\nthe booker2 function has its own closure`);
+const booker2 = secureBooking();
+booker2();
+booker();
+
+console.log(`\nClosures cannot be accessed directly, but they can be observed`);
+console.dir(booker);
